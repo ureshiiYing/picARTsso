7ffea1f2e8403e6e8955c04fabe80944932296d0
ureshiiYing <ureshiiYing@users.noreply.github.com>
2020-07-23 01:46:55 +0800

    Adding 2 versions of spinner animations
    used the spinner in waiting room

---
+++ Assets/Scripts/SpinnerRotation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnerRotation : MonoBehaviour
+{
+    public RectTransform mainIcon;
+    public float timeStep;
+    public float oneStepAngle;
+
+    private float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - startTime >= timeStep)
+        {
+            Vector3 iconAngle = mainIcon.localEulerAngles;
+            iconAngle.z += oneStepAngle;
+
+            mainIcon.localEulerAngles = iconAngle;
+
+            startTime = Time.time;
+        }
+    }
+}
