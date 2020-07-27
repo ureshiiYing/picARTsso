05653d5b7c0caec0e5f05ed7a7950ee874c8e36b
ureshiiYing <ureshiiYing@users.noreply.github.com>
2020-07-27 13:54:56 +0800

    Implemented exit game function

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitGame : MonoBehaviour
+{
+    public Button exitButton;
+
+    public void ExitThisGame()
+    {
+        Application.Quit();
+    }
+}
